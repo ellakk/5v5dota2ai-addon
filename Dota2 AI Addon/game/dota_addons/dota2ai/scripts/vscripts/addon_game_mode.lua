4f4757101bb302b5e4c922db2b37993cf722d725
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-02-10 20:30:31 +0100

    Reformat the code
    Using lua-fmt we fix the following:
    - Tabs have been converted to spaces, previously they were mixed
    - Split long lines
    - Split one-line functions to be more readable
    
    Using vim we changed line endings from dos to unix

--- Dota2 AI Addon/game/dota_addons/dota2ai/scripts/vscripts/addon_game_mode.lua
+++ Dota2 AI Addon/game/dota_addons/dota2ai/scripts/vscripts/addon_game_mode.lua
@@ -1,19 +1,14 @@
- 
-
 if Dota2AI == nil then
-	_G.Dota2AI = class({})
+    _G.Dota2AI = class({})
 end
-
-
-
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Required .lua files, which just exist to help organize functions contained in our addon.  Make sure to call these beneath the mode's class creation.
 ------------------------------------------------------------------------------------------------------------------------------------------------------
-require( "json_helpers" )
-require( "hero_tools" )
-require( "utility_functions" )
-require( "events" )
+require("json_helpers")
+require("hero_tools")
+require("utility_functions")
+require("events")
 
 --------------------------------------------------------------------------------
 -- ACTIVATE
@@ -21,40 +16,37 @@ require( "events" )
 function Activate()
     GameRules.Dota2AI = Dota2AI()
     GameRules.Dota2AI:InitGameMode()
-	
 end
 
-
-require ("config")
+require("config")
 
 --------------------------------------------------------------------------------
 -- INIT
 --------------------------------------------------------------------------------
 function Dota2AI:InitGameMode()
-	
+    print("Initializing Dota2 AI mode")
+    CustomNetTables:SetTableValue("game_state", "base_url", {value = Dota2AI.baseURL})
+    local GameMode = GameRules:GetGameModeEntity()
 
-	print( "Initializing Dota2 AI mode" )
-	CustomNetTables:SetTableValue( "game_state", "base_url", { value =  Dota2AI.baseURL} )
-	local GameMode = GameRules:GetGameModeEntity()
-	
-	GameMode:SetBotThinkingEnabled( true ) -- the ConVar is currently disabled in C++
-	--GameMode:SetFogOfWarDisabled(true)
-	
-	GameRules:SetShowcaseTime( 0 )
-	GameRules:SetStrategyTime( 0 )
-	GameRules:SetStrategyTime( 0 )
-	GameRules:SetHeroSelectionTime( 0 )
-	GameRules:SetCustomGameSetupTimeout( Dota2AI.ConfigUITimeout ) -- skip the custom team UI with 0, or do indefinite duration with -1
-	
-	
-	
-	-- Events
-	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( Dota2AI, 'OnGameRulesStateChange' ), self )
-	ListenToGameEvent("player_chat", Dynamic_Wrap(Dota2AI, 'OnPlayerChat'), self)
-	
+    GameMode:SetBotThinkingEnabled(true) -- the ConVar is currently disabled in C++
+    --GameMode:SetFogOfWarDisabled(true)
 
-	CustomGameEventManager:RegisterListener( "base_url_changed", function(...) return self:OnBaseURLChanged( ... ) end  )	
- end
+    GameRules:SetShowcaseTime(0)
+    GameRules:SetStrategyTime(0)
+    GameRules:SetStrategyTime(0)
+    GameRules:SetHeroSelectionTime(0)
+    GameRules:SetCustomGameSetupTimeout(Dota2AI.ConfigUITimeout) -- skip the custom team UI with 0, or do indefinite duration with -1
 
+    -- Events
+    ListenToGameEvent("game_rules_state_change", Dynamic_Wrap(Dota2AI, "OnGameRulesStateChange"), self)
+    ListenToGameEvent("player_chat", Dynamic_Wrap(Dota2AI, "OnPlayerChat"), self)
 
-print( "Dota2 AI  game mode loaded." )
+    CustomGameEventManager:RegisterListener(
+        "base_url_changed",
+        function(...)
+            return self:OnBaseURLChanged(...)
+        end
+    )
+end
+
+print("Dota2 AI  game mode loaded.")
