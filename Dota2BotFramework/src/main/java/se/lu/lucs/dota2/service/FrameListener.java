b2669704c3c253921daac094f17ac7eb5becc074
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-03-04 14:15:07 +0100

    Remove java framework

--- Dota2BotFramework/src/main/java/se/lu/lucs/dota2/service/FrameListener.java
+++
@@ -1,9 +0,0 @@
-package se.lu.lucs.dota2.service;
-
-import se.lu.lucs.dota2.framework.bot.Bot.Command;
-import se.lu.lucs.dota2.framework.game.World;
-
-@FunctionalInterface
-public interface FrameListener {
-    Command update( World w );
-}
