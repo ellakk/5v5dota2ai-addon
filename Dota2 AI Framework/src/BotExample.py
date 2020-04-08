6bc0e5c0d8917cc413ba821f840f679c6706477b
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-04-08 15:42:32 +0200

    make example bot attack enemies in range

--- Dota2 AI Framework/src/BotExample.py
+++ Dota2 AI Framework/src/BotExample.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import random
 
 class BotExample:
     def __init__(self, world):
@@ -31,23 +32,46 @@ class BotExample:
         if hero.getName() == "npc_dota_hero_chen":
             self.actions_chen(hero)
 
+    def attack_if_in_range(self, hero):
+        enemies = self.world.get_enemies_in_attack_range(hero)
+        if enemies:
+            target = random.choice(enemies)
+            hero.attack(self.world.get_id(target))
+            return True
+        return False
+
     def actions_brewmaster(self, hero):
+        if self.attack_if_in_range(hero):
+            return
+
         tower = self.world.find_entity_by_name("dota_goodguys_tower1_mid")
         hero.move(*tower.getOrigin())
 
     def actions_pudge(self, hero):
+        if self.attack_if_in_range(hero):
+            return
+
         tower = self.world.find_entity_by_name("dota_goodguys_tower1_bot")
         hero.move(*tower.getOrigin())
 
     def actions_abyssal_underlord(self, hero):
+        if self.attack_if_in_range(hero):
+            return
+
         tower = self.world.find_entity_by_name("dota_goodguys_tower1_top")
         hero.move(*tower.getOrigin())
 
     def actions_lina(self, hero):
+        if self.attack_if_in_range(hero):
+            return
+
         pudge = self.world.find_entity_by_name("npc_dota_hero_pudge")
         hero.move(*pudge.getOrigin())
 
     def actions_chen(self, hero):
+        if self.attack_if_in_range(hero):
+            return
+
         abyssal_underlord = self.world.find_entity_by_name(
             "npc_dota_hero_abyssal_underlord"
         )
