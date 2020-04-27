977e8312f7f97308e9146e62a0f31f01bde43a52
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-04-27 11:48:05 +0200

    Start development of exp 41

--- Dota2 AI Framework/src/bots/Experiment41.py
+++ Dota2 AI Framework/src/bots/Experiment41.py
@@ -1,9 +1,9 @@
 # noTarget
-# mirana Leap
-# Slardar Slithereen
-# Zeus Thundergods wrath
-# Brewmaster Thunder clap
-# Chen Hand of God
+# mirana Leap #
+# Slardar Slithereen #
+# Clinkz Skeleton walk #
+# Brewmaster Thunder clap #
+# Alchemist  # Unstable concoction
 
 
 class Experiment41:
@@ -11,11 +11,27 @@ class Experiment41:
         self.party = [
             "npc_dota_hero_mirana",
             "npc_dota_hero_slardar",
-            "npc_dota_hero_zuus",
+            "npc_dota_hero_clinkz",
             "npc_dota_hero_brewmaster",
-            "npc_dota_hero_chen"
+            "npc_dota_hero_alchemist"
         ]
+
+        self.hero_no_targets_abilities = {
+            "npc_dota_hero_mirana": 2,      # Leap
+            "npc_dota_hero_slardar": 1,     # Slithereen
+            "npc_dota_hero_clinkz": 2,      # Skeleton walk
+            "npc_dota_hero_brewmaster": 0,  # Thunder clap
+            "npc_dota_hero_alchemist": 1    # Unstable concoction
+        }
         self.world = world
 
-        def cast_no_target(self, hero):
-            pass
+    def initialize(self, heroes):
+        print("Starting Experiment 41:")
+
+    def actions(self, hero):
+        if self.world.gameticks > 10:
+            self.maybe_use_no_target(hero)
+
+    def maybe_use_no_target(self, hero):
+        ability_index = self.hero_no_targets_abilities[hero.getName()]
+        hero.cast(ability_index)
