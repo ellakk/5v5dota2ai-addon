78688a393b2dab640eb6f8dc6bc843943e740561
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-04-29 15:57:21 +0200

    Set a fakename and team for trees

--- Dota2 AI Framework/src/game/Tree.py
+++ Dota2 AI Framework/src/game/Tree.py
@@ -5,4 +5,11 @@ from src.game.BaseEntity import BaseEntity
 
 class Tree(BaseEntity):
     def __init__(self, data):
+        data['name'] = 'tree'
+        data['team'] = 4
         super().__init__(data)
+
+    def setData(self, data):
+        data['name'] = 'tree'
+        data['team'] = 4
+        super().setData(data)
