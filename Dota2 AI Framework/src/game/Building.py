a5e1ca28bd0ec8fc6a2dc2ecb356b1a1d1f06926
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2020-05-06 10:12:52 +0200

    Restructure project, move framework to sep repo

--- Dota2 AI Framework/src/game/Building.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from src.game.BaseNPC import BaseNPC
-
-
-class Building(BaseNPC):
-    def __init__(self, data):
-        super().__init__(data)
--- Dota2 AI Framework/src/game/Tower.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from src.game.Building import Building
-
-
-class Tower(Building):
-    def __init__(self, data):
-        super().__init__(data)
